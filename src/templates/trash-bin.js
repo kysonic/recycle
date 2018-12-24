b70cd7606da4d839a1dc5b80e1c6812f746ef0d2
Miroshnichenko Anton <anton.miroshnichenko@omnigon.com>
2018-12-24 21:27:57 +0300

    Trash bins completed

--- src/templates/trash-bin.js
+++ src/templates/trash-bin.js
@@ -6,56 +6,60 @@ import '../components/trash-bin';
 import '../components/trash-bin-collider';
 
 
-AFRAME.registerTemplate('trash-bin', ({type, position}) => `
-    <a-entity id="trash-bin-${type}" position="${position}" trash-bin="type: ${type}" scale="1.5 1.5 1.5">
+AFRAME.registerTemplate('trash-bin', ({type, position, scale="1.5 1.5 1.5"}) => `
+    <a-entity id="trash-bin-${type}" position="${position}" trash-bin="type: ${type}" scale="${scale}">
         <a-box class="left" static-body 
         position="-0.12 0.03 0" 
         width="0.01" 
-        height="0.45" 
-        depth="0.25"
+        height="0.7" 
+        depth="0.4"
         rotation="0 3 4"
          material="transparent: true; opacity: 0"
         ></a-box>
         <a-box class="right" static-body 
         position="0.13 0.03 0" 
         width="0.01" 
-        height="0.45" 
+        height="0.5" 
         depth="0.25"
         rotation="0 3 -4"
         material="transparent: true; opacity: 0"
+        scale="${scale}"
         ></a-box>
         <a-box class="forward" static-body 
         position="0 0.03 0.13" 
         width="0.25" 
-        height="0.45" 
+        height="0.47" 
         depth="0.01"
         rotation="4 0 0"
          material="transparent: true; opacity: 0"
+         scale="${scale}"
         ></a-box>
         <a-box class="forward" static-body 
         position="0 0.03 0.13" 
         width="0.25" 
-        height="0.45" 
+        height="0.5" 
         depth="0.01"
         rotation="4 0 0"
         material="transparent: true; opacity: 0"
+        scale="${scale}"
         ></a-box>
         <a-box class="back" static-body 
         position="0 0.03 -0.13" 
         width="0.25" 
-        height="0.45" 
+        height="0.47" 
         depth="0.01"
         rotation="-4 0 0"
         material="transparent: true; opacity: 0"
+        scale="${scale}"
         ></a-box>
         <a-box class="collider" 
                trash-bin-collider
                static-body 
                collision-filter="collisionForces: false"
                position="0 0 0" 
-               width="0.1"
+               width="0.2"
                height="0.1"
-               depth="0.1"
+               depth="0.2"
                color="red"
                trasnparent="true"
                opacity="0">
