1ae07f73402a1bbf63891514080b566fc8d7969a
Anton Miroshnochenko <soooyc@gmail.com>
2023-08-12 13:19:39 +0300

    game over

--- src/templates/routes/game-over.js
+++ src/templates/routes/game-over.js
@@ -1,21 +1,33 @@
 AFRAME.registerTemplate(
   'game-over',
   html`
-    <a-entity 
-        position="-2.2 2 -4" 
-        material="color: #90dfdf;" 
-        text-geometry="value: GAME OVER; font: #Roboto; align: center"></a-entity>
-    <a-entity 
-        position="-1.9 1.5 -4" 
-        material="color: #90dfdf;" 
-        text-geometry="value: YOUR SCORE:; font: #Roboto; align: center; size: 0.3"></a-entity> 
-    <a-entity 
-        position="1 1.51 -4" 
-        material="color: #90dfdf;" 
-        bind="text-geometry.value: score"
-        text-geometry="value: 0; font: #Roboto; align: center; size: 0.3"></a-entity>   
-    
-    </a-plane>    
-    <a-template name="button" options="text: RESTART; position: 0 0.8 -4; event: start-game; color: #5e9696; width: 1.3"></a-template>
-`,
+    <a-text
+      position="0 2 -4"
+      scale="2 2 2"
+      align="center"
+      value="GAME OVER"
+      color="white"
+      material="color: white;"
+    ></a-text>
+    <a-text
+      position="-0.5 1.6 -4"
+      scale="1 1 1"
+      align="center"
+      value="YOUR SCORE: "
+      color="white"
+      material="color: white;"
+    ></a-text>
+    <a-text
+      position="0.5 1.6 -4"
+      scale="1 1 1"
+      align="center"
+      bind="text.value: score"
+      color="white"
+      material="color: white;"
+    ></a-text>
+    <a-template
+      name="button"
+      options="text: RESTART; position: 0 0.8 -4; event: start-game; color: #5e9696; width: 1.3"
+    ></a-template>
+  `,
 );
